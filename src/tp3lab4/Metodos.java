9f699760b20013e2a810b16fad6959d3b3de0ce7
Leandro Sorolla <leandrosorolla@gmail.com>
2020-04-02 12:02:23 -0300

    tp3

---
+++ src/tp3lab4/Metodos.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3lab4;
+
+import com.mongodb.BasicDBObject;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Indexes;
+
+import com.mongodb.client.result.DeleteResult;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+
+/**
+ *
+ * @author Leandro
+ */
+public class Metodos {
+
+
+    public static void imprimeAmericas(MongoCollection collection) {
+        System.out.println("\nPaíses con región Americas: ");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("region", "Americas");
+
+        MongoCursor<Document> cursor = collection.find(query).iterator();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next().toJson());
+        }
+
+    }
+
+    public static void regionPoblacion(MongoCollection collection) {
+        System.out.println("\nPaises con región Americas y población mayor a 100000000");
+
+        BasicDBObject criteria = new BasicDBObject();
+        criteria.put("region", "Americas");
+        criteria.put("poblacion", new Document("$gt", 50000000));
+
+        MongoCursor<Document> cursor = collection.find(criteria).iterator();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+
+    }
+
+    public static void distintoAfrica(MongoCollection collection) {
+        System.out.println("\nPaises con región distinta a Africa");
+
+        BasicDBObject query = new BasicDBObject();
+        query.append("region", new BasicDBObject("$ne", "Africa"));
+
+        MongoCursor<Document> cursor = collection.find(query).iterator();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next().toJson());
+        }
+    }
+
+    public static void updateEgypt(MongoCollection collection) {
+        System.out.println("\nBuscando Egypt para actualizar...");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("nombrePais", "Egypt");
+
+        Document found = (Document) collection.find(new Document("nombrePais", "Egypt")).first();
+
+        if (found != null) {
+
+            Bson update = new Document("nombrePais", "Egipto").append("poblacion", 95000000);
+            Bson updateEgypt = new Document("$set", update);
+            collection.updateOne(found, updateEgypt);
+            System.out.println("Egypt actualizado");
+
+        }
+
+    }
+
+    public static void delete258(MongoCollection collection) {
+        System.out.println("\nBuscando código 258 para eliminarlo de la colección...");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("codigoPais", 258);
+        DeleteResult result = collection.deleteOne(query);
+        System.out.println("Resultado de la operación: " + result.getDeletedCount());
+
+    }
+
+    public static void poblacionMayorqueMenorque(MongoCollection collection) {
+        System.out.println("\nPoblación mayor que 50000000 y menor que 150000000");
+
+        FindIterable<Document> iterable = collection.find(
+                new Document("poblacion", new Document("$gt", 50000000).append("$lt", 150000000)));
+
+        MongoCursor<Document> cursor = iterable.iterator();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next().toJson());
+        }
+
+    }
+
+    public static void ordenadoPorNombre(MongoCollection collection) {
+        System.out.println("\nPaises ordenado por nombre (Ascendente): ");
+
+        MongoCursor<Document> cursor = collection.find().sort(new BasicDBObject("nombrePais", 1)).iterator();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next().toJson());
+        }
+
+    }
+    public static void codigoAindex(MongoCollection collection){
+              System.out.println("\nConvirtiendo a codigoPais en INDEX");
+        try {
+            collection.createIndex(Indexes.ascending("codigoPais"));
+
+            System.out.println("Index creado para codigoPais");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
---
+++ src/tp3lab4/Tp3lab4.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3lab4;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bson.Document;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Tp3lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Logger mongoLog = Logger.getLogger("org.mongodb.drive");
+
+        mongoLog.setLevel(Level.SEVERE);
+        String uri = "mongodb+srv://leandro:leandro@leandro-rptpo.mongodb.net/test?retryWrites=true&w=majority";
+        MongoClientURI clientURI = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(clientURI);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("paises_db");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("paises");
+        Document document = new Document();
+
+        String datosJson = "https://restcountries.eu/rest/v2/callingcode/";
+
+        JSONParser parser = new JSONParser();
+ for (int i = 1; i <= 300; i++) {
+
+            try {
+
+                URL link = new URL(datosJson + i);
+                URLConnection yc = link.openConnection();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+                JSONArray arrjson = (JSONArray) parser.parse(in.readLine());
+                if (arrjson != null) {
+                    for (Object object : arrjson) {
+                        JSONObject paisJson = (JSONObject) object;
+                        document.append("codigoPais", i);
+                        document.append("nombrePais", paisJson.get("name"));
+                        document.append("capitalPais", paisJson.get("capital"));
+                        document.append("region", paisJson.get("region"));
+                        document.append("poblacion", paisJson.get("population"));
+                        List coorGeo = (List) paisJson.get("latlng");
+                        document.append("latitud", (double) coorGeo.get(0));
+                        document.append("longitud", (double) coorGeo.get(1));
+                        document.append("superficie", paisJson.get("area"));
+                        collection.insertOne(document);
+                        document.clear();
+                        System.out.println("Pais encontrado, código: " + i);
+                    }
+                } else {
+                    continue;
+                }
+                in.close();
+            } catch (Exception e) {
+                System.out.println("No existe un pais con el código: " + i);
+            }
+        }
+         System.gc();
+         Metodos met = new Metodos();
+         met.imprimeAmericas(collection);
+         met.regionPoblacion(collection);
+         met.distintoAfrica(collection);
+         met.updateEgypt(collection);
+         met.delete258(collection);
+         met.poblacionMayorqueMenorque(collection);
+         met.ordenadoPorNombre(collection);
+         met.codigoAindex(collection);
+         
+    }
+
+}
